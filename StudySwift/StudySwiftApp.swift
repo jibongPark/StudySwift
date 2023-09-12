4644e8355faeb256f67eaf3528ad10fc4ef86417
jbpark <bongjibong22@naver.com>
2023-09-12 20:50:49 +0900

    Initial Commit

---
+++ StudySwift/StudySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftApp.swift
+//  StudySwift
+//
+//  Created by 박지봉 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
